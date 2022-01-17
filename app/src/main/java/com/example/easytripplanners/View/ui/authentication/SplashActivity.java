7c08c7d697fae1d2f349410c443b487b7b9cce30
seifoozzz <seifoozzz@icloud.com>
2022-01-17 21:09:52 +0200

    login and sign up

---
+++ app/src/main/java/com/example/easytripplanners/View/ui/authentication/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.easytripplanners.View.ui.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.easytripplanners.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
